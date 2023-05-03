9399407ee4ec07ef3acb181b2fdb9409f0c1cae6
Pavel Dubsky <pavel.dubsky@padutronics.com>
2023-05-03 22:16:35 +0200

    Register type Padutronics.Conversion.Converters.Geometry.Point2ToStringConverter<System.SByte>

---
+++ Source/Padutronics.Conversion.Converters.Geometry.DependencyInjection/Modules/GeometryConvertersContainerModule.cs
@@ -0,0 +1,19 @@
+using Padutronics.DependencyInjection;
+using Padutronics.Geometry;
+using System.Numerics;
+
+namespace Padutronics.Conversion.Converters.Geometry.DependencyInjection.Modules;
+
+public sealed class GeometryConvertersContainerModule : IContainerModule
+{
+    public void Load(IContainerBuilder containerBuilder)
+    {
+        RegisterPoint2ToStringConverter<sbyte>(containerBuilder);
+    }
+
+    private void RegisterPoint2ToStringConverter<T>(IContainerBuilder containerBuilder)
+        where T : INumber<T>
+    {
+        containerBuilder.For<IConverter<Point2<T>, string>>().Use<Point2ToStringConverter<T>>().InstancePerDependency();
+    }
+}
